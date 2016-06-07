3d236c7243e07e1c5ce7846758873b12b288770f
rystoli <ryan.m.stolier@gmail.com>
2016-06-06 21:59:23 -0400

    fixed csv export roipairsim

--- rymvpa/roi_wraps.py
+++ rymvpa/roi_wraps.py
@@ -334,5 +334,6 @@ def roi_pairsim_nSs(data, roi_mask_nii_path, pairs, pairwise_metric='correlation
     '''
 
     res = dict ( [ (s,roi_pairsim_1Ss(data[s],roi_mask_nii_path,pairs,pairwise_metric=pairwise_metric,fisherz=fisherz)) for s in data ] )
+    res = dict([(s, dict([(t,float(res[s][t])) for t in res[s]])) for s in res])
     if csv == 1: pd.DataFrame(res).to_csv(csvout,sep=',')
     return res
